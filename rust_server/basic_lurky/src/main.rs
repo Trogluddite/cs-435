cca16d59b8900095e80945f80378dcef17b6767f
Joe Burchett <5685541+Trogluddite@users.noreply.github.com>
2024-09-23 22:43:17 -0700

    some wip

--- rust_server/basic_lurky/src/main.rs
+++ rust_server/basic_lurky/src/main.rs
@@ -1,14 +1,17 @@
-use std::io::{BufRead, BufReader, BufWriter, Write};
+use std::io::{BufWriter, Write};
 use std::net::{TcpListener, TcpStream};
 
 fn handle_client(stream: TcpStream) {
     let mut client_writer: BufWriter<&TcpStream> = BufWriter::new(&stream);
 
     let x:[u8;7] = [14, 2, 3, 0, 0, 0, 0];
-    let y:[u8;7] = [123, 7, 6, 5, 4, 3, 1];
-    client_writer.write_all(&x);
 
-    client_writer.write_all(&y);
+    let y = [7, 4, 1, 0];
+    let err = "A SAD HAS HAPPEN";
+
+    let _ = client_writer.write_all(&x);
+    let _ = client_writer.write_all(&y);
+    let _ = client_writer.write_all(&err.as_bytes());
 
 }
 
