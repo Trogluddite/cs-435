0f3b602e1c320208a2b46a804afad6c9f527beec
Joe Burchett <5685541+Trogluddite@users.noreply.github.com>
2024-11-16 23:56:55 -0800

    set up basic app state & screen switching

---
+++ rust_client/lurkalyzer/src/app.rs
@@ -0,0 +1,37 @@
+pub enum Screen{
+    Main,
+    Connfiguration,
+    RawMode,
+    LurkMode,
+}
+
+pub struct App {
+    pub current_screen: Screen,
+    pub server_address: String,
+    pub server_port: u16,
+    pub server_connected: bool,
+    // in buffer
+    // out buffer
+    // other app state?
+}
+
+impl App {
+    pub fn new() -> App {
+        App {
+            current_screen: Screen::Main,
+            server_address: String::new(),
+            server_port: 0,
+            server_connected: false,
+        }
+    }
+
+    pub fn set_server(&mut self, ip: String, port: u16) {
+        self.server_address = ip;
+        self.server_port = port;
+        //connect here? just seetting up screens now
+    }
+
+    pub fn switch_screen(&mut self, target: Screen) {
+        self.current_screen = target;
+    }
+}
--- rust_client/lurkalyzer/src/main.rs
+++ rust_client/lurkalyzer/src/main.rs
@@ -1,32 +1,74 @@
-use std::io;
+use std::{error::Error, io};
 
 use ratatui::{
-    crossterm::event::{self, KeyCode, KeyEventKind},
-    style::Stylize,
-    widgets::Paragraph,
-    DefaultTerminal,
+    backend::{Backend, CrosstermBackend},
+    crossterm::{
+        event::{self, Event, KeyCode, KeyEventKind},
+        execute,
+        terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+    },
+    Terminal,
 };
 
-fn main() -> io::Result<()> {
-    let mut terminal = ratatui::init();
-    terminal.clear()?;
-    let app_result = run(terminal);
-    ratatui::restore();
-    app_result
+mod app;
+//mod ui;
+
+use crate::{
+    app::{App, Screen},
+    //ui::ui,
+};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    //set up terminal
+    enable_raw_mode()?;
+    let mut stderr = io::stderr(); //terminal defaults to stderr/stdout to same stream
+    execute!(stderr, EnterAlternateScreen)?;
+    let backend = CrosstermBackend::new(stderr);
+    let mut terminal = Terminal::new(backend)?;
+
+    //create app & run it
+    let mut app = App::new();
+    let res = run_app(&mut terminal, &mut app);
+
+    //restore terminal
+    disable_raw_mode()?;
+    execute!(
+        terminal.backend_mut(),
+        LeaveAlternateScreen,
+    )?;
+    terminal.show_cursor()?;
+
+    if let Err(err) = res{
+        println!("{err:?}");
+    }
+
+    Ok(())
 }
 
-fn run(mut terminal: DefaultTerminal) -> io::Result<()> {
+fn run_app<B: Backend>(terminal: &mut Terminal<B>, app: &mut App) -> io::Result<bool> {
     loop {
-        terminal.draw(|frame| {
-            let greeting = Paragraph::new("Hello Ratatui! (press 'q' to quit)")
-                .white()
-                .on_blue();
-            frame.render_widget(greeting, frame.area());
-        })?;
-
-        if let event::Event::Key(key) = event::read()? {
-            if key.kind == KeyEventKind::Press && key.code == KeyCode::Char('q') {
-                return Ok(());
+        //terminal.draw(|frame| ui(frame, app))?;
+        if let Event::Key(key) = event::read()? {
+            if key.kind == KeyEventKind::Release{
+                // we only care about KeyEventKind::press
+                continue;
+            }
+            match key.code {
+                KeyCode::Char('m') => {
+                    app.current_screen = Screen::Main;
+                }
+                KeyCode::Char('c') => {
+                    app.current_screen = Screen::Connfiguration;
+                }
+                KeyCode::Char('r') => {
+                    app.current_screen = Screen::RawMode;
+                }
+                KeyCode::Char('l') => {
+                    app.current_screen = Screen::LurkMode;
+                }
+                // add exit screen
+                // add type/edit handlers
+                _ => {}
             }
         }
     }
