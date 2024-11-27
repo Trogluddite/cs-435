bad8a89a898b765f6a581e7b128d1ba70c4b7c6d
Joe Burchett <5685541+Trogluddite@users.noreply.github.com>
2024-11-26 22:10:04 -0800

    a more complex layout

--- rust_client/lurkalyzer/src/ui.rs
+++ rust_client/lurkalyzer/src/ui.rs
@@ -13,22 +13,40 @@ use crate::app::{App, CurrentScreen};
 
 #[allow(unused_variables)] //FIXME
 pub fn ui(frame: &mut Frame, app: &App){
-    let layout_chunks = Layout::default()
+    let outer_layout = Layout::default()
         .direction(Direction::Vertical)
         .constraints([
-            Constraint::Percentage(100),
+            Constraint::Percentage(75),
+            Constraint::Percentage(25),
         ])
         .split(frame.area());
+    let inner_layout = Layout::default()
+        .direction(Direction::Horizontal)
+        .constraints([
+            Constraint::Percentage(50),
+            Constraint::Percentage(50)
+        ])
+        .split(outer_layout[1]);
 
     let title = Line::from(" Lurkalyzer ");
     let footer = Line::from(" Press (q) to quit ");
-
     let block = Block::bordered()
         .title(title.centered())
         .title_bottom(footer.centered())
         .border_set(border::DOUBLE);
+    frame.render_widget(block, outer_layout[0]);
 
-    frame.render_widget(block, layout_chunks[0]);
+    let cfg_title = Line::from(" Config ");
+    let incomming_title = Line::from(" Incomming ");
+    let cfg_block = Block::bordered()
+        .title(cfg_title.centered())
+        .border_set(border::DOUBLE);
+    frame.render_widget(cfg_block, inner_layout[0]);
+
+    let incomming_block = Block::bordered()
+        .title(incomming_title.centered())
+        .border_set(border::DOUBLE);
+    frame.render_widget(incomming_block, inner_layout[1]);
 }
 
 #[allow(dead_code)]
